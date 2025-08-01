bf473e7b743363eba06d098bcc8dd52980a9d386
Felipe Montoya <felipe.montoya@edunext.co>
2025-08-01 17:58:58 -0500

    feat: moving services into a new workflows module --- the current model is only a memory object to experiment

--- backend/openedx_ai_extensions/api/v1/urls.py
+++ backend/openedx_ai_extensions/api/v1/urls.py
@@ -2,10 +2,10 @@
 Version 1 API URLs
 """
 from django.urls import path
-from .pipelines.views import AIGenericPipelinesView
+from .pipelines.views import AIGenericWorkflowView
 
 app_name = 'v1'
 
 urlpatterns = [
-    path('pipelines/', AIGenericPipelinesView.as_view(), name='ai_pipelines'),
+    path('workflows/', AIGenericWorkflowView.as_view(), name='ai_pipelines'),
 ]
