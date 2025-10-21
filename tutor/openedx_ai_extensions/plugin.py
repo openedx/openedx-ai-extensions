1a041f2d94dad4b62d04f5b3f4ab1d7226136309
Felipe Montoya <felipe.montoya@edunext.co>
2025-10-21 12:09:08 -0500

    feat: support local npm installing the ui slots using tutor-mfe

--- tutor/openedx_ai_extensions/plugin.py
+++ tutor/openedx_ai_extensions/plugin.py
@@ -1,11 +1,26 @@
-from tutor import hooks
 import os
 from glob import glob
+from pathlib import Path
 
 import importlib_resources
-from tutormfe.hooks import PLUGIN_SLOTS
+from tutor import hooks
+from tutormfe.hooks import MFE_APPS, PLUGIN_SLOTS
+
 from .__about__ import __version__
 
+
+########################
+# Plugin path management
+########################
+
+PLUGIN_DIR = Path(__file__).parent
+REPO_ROOT = PLUGIN_DIR.parent.parent
+FRONTEND_PATH = REPO_ROOT / "frontend"
+
+# Makes the UI Slots code available for local install during the build process
+hooks.Filters.DOCKER_BUILD_COMMAND.add_items([
+    "--build-context", f"ai-extensions-frontend={str(FRONTEND_PATH)}"
+])
 
 @hooks.Filters.IMAGES_BUILD_MOUNTS.add()
 def _mount_sample_plugin(mounts, path):
@@ -13,7 +28,31 @@ def _mount_sample_plugin(mounts, path):
     mounts += [("openedx-ai-extensions/backend", "/openedx/openedx-ai-extensions/backend")]
     return mounts
 
-
+# Actually connects the patch files as tutor env patches
 for path in glob(str(importlib_resources.files("openedx_ai_extensions") / "patches" / "*")):
     with open(path, encoding="utf-8") as patch_file:
         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+
+########################
+# UI Slot configurations
+########################
+
+PLUGIN_SLOTS.add_items(
+    [
+        (
+            "learning",
+            "org.openedx.frontend.learning.unit_title.v1",
+            """
+          {
+            op: PLUGIN_OPERATIONS.Insert,
+            widget: {
+                id: 'ai-assist-button',
+                priority: 10,
+                type: DIRECT_PLUGIN,
+                RenderWidget: GetAIAssistanceButton,
+            },
+          }""",
+        ),
+    ]
+)
