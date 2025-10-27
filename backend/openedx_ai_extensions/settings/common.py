1b88c011723187f8907471542b0d6e77f037e20b
henrrypg <henrry.pulgarin@edunext.co>
2025-10-27 08:57:08 -0500

    feat: add settings defaults

--- backend/openedx_ai_extensions/settings/common.py
+++ backend/openedx_ai_extensions/settings/common.py
@@ -2,6 +2,10 @@
 Common settings for the openedx_ai_extensions application.
 """
 import os
+import logging
+from copy import deepcopy
+
+logger = logging.getLogger(__name__)
 
 
 def plugin_settings(settings):  # pylint: disable=unused-argument
@@ -11,11 +15,19 @@ def plugin_settings(settings):  # pylint: disable=unused-argument
     Args:
         settings (dict): Django settings object
     """
-    if not hasattr(settings, "OPENEDX_AI_EXTENSIONS"):
-        settings.OPENEDX_AI_EXTENSIONS = os.getenv("OPENEDX_AI_EXTENSIONS", default={
-            "default": {
-                "API_KEY": "",
-                "LITELLM_MODEL": "gpt-5-mini",
-                "TEMPERATURE": 1,
-            }
-        })
+    CONFIG_DEFAULTS = {
+        "default": {
+            "API_KEY": "put_your_api_key_here",
+            "LITELLM_MODEL": "gpt-5-mini",
+            "TEMPERATURE": 1,
+        }
+    }
+    config = deepcopy(CONFIG_DEFAULTS)
+    if hasattr(settings, "OPENEDX_AI_EXTENSIONS"):
+        for section, values in settings.OPENEDX_AI_EXTENSIONS.items():
+          if section in config:
+              logger.warning(f"OpenedX AI Extensions settings: {settings.OPENEDX_AI_EXTENSIONS}")
+              config[section].update(values)
+          else:
+              config[section] = values
+    settings.OPENEDX_AI_EXTENSIONS = config
