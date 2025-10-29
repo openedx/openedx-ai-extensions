d6208d0b7202074e75e6402ed6e2b173c9917eb3
henrrypg <henrry.pulgarin@edunext.co>
2025-10-29 15:55:44 -0500

    refactor: move mcp url to v1 api

--- backend/openedx_ai_extensions/api/urls.py
+++ backend/openedx_ai_extensions/api/urls.py
@@ -3,13 +3,10 @@ Main API URLs for openedx-ai-extensions
 """
 
 from django.urls import include, path
-from mcp_server.views import MCPServerStreamableHttpView
 
-from ..mcp.server import mcp
 
 app_name = "openedx_ai_extensions_api"
 
 urlpatterns = [
     path("v1/", include("openedx_ai_extensions.api.v1.urls", namespace="v1")),
-    path("mcp", MCPServerStreamableHttpView.as_view(mcp_server=mcp), name="mcp_server"),
 ]
