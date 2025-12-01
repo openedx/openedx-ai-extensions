b3cb6e00d0e63388d3973f657d747e4e1b714914
henrrypg <henrry.pulgarin@edunext.co>
2025-12-01 17:18:16 -0500

    feat: allow include mcp configuration via settings

--- backend/openedx_ai_extensions/processors/litellm_base_processor.py
+++ backend/openedx_ai_extensions/processors/litellm_base_processor.py
@@ -33,6 +33,24 @@ class LitellmProcessor:
         if "MAX_TOKENS" in settings.AI_EXTENSIONS[self.config_profile]:
             self.extra_params["max_tokens"] = settings.AI_EXTENSIONS[self.config_profile]["MAX_TOKENS"]
 
+        self.mcp_configs = {}
+        if hasattr(settings, "AI_EXTENSIONS_MCP_CONFIGS"):
+            allowed_mcp_configs = self.config.get("mcp_configs", [])
+            if allowed_mcp_configs:
+                self.mcp_configs = {
+                    key: value
+                    for key, value in settings.AI_EXTENSIONS_MCP_CONFIGS.items()
+                    if key in allowed_mcp_configs
+                }
+                self.extra_params["tools"] = [
+                    {
+                        "type": "mcp",
+                        "server_label": key,
+                        **value,
+                    }
+                    for key, value in self.mcp_configs.items()
+                ]
+
         if not self.api_key:
             logger.error("AI API key not configured")
 
