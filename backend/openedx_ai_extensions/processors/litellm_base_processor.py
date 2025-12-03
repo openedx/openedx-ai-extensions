bd22eefcde604b8cbf843b97fa05c44a3dfb7e0b
henrrypg <henrry.pulgarin@edunext.co>
2025-12-03 10:47:28 -0500

    fix: mode setting patch

--- backend/openedx_ai_extensions/processors/litellm_base_processor.py
+++ backend/openedx_ai_extensions/processors/litellm_base_processor.py
@@ -37,22 +37,21 @@ class LitellmProcessor:
             self.extra_params[key.lower()] = value
 
         self.mcp_configs = {}
-        if hasattr(settings, "AI_EXTENSIONS_MCP_CONFIGS"):
-            allowed_mcp_configs = self.config.get("mcp_configs", [])
-            if allowed_mcp_configs:
-                self.mcp_configs = {
-                    key: value
-                    for key, value in settings.AI_EXTENSIONS_MCP_CONFIGS.items()
-                    if key in allowed_mcp_configs
+        allowed_mcp_configs = self.config.get("mcp_configs", [])
+        if allowed_mcp_configs:
+            self.mcp_configs = {
+                key: value
+                for key, value in settings.AI_EXTENSIONS_MCP_CONFIGS.items()
+                if key in allowed_mcp_configs
+            }
+            self.extra_params["tools"] = [
+                {
+                    "type": "mcp",
+                    "server_label": key,
+                    **value,
                 }
-                self.extra_params["tools"] = [
-                    {
-                        "type": "mcp",
-                        "server_label": key,
-                        **value,
-                    }
-                    for key, value in self.mcp_configs.items()
-                ]
+                for key, value in self.mcp_configs.items()
+            ]
 
         if not self.api_key:
             logger.error("AI API key not configured")
