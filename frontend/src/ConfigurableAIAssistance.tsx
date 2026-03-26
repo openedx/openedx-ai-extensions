05a92be0f334763b927a7a9516be135d2c24552a
pavilion4ik <vladearth7@gmail.com>
2026-03-26 21:00:12 +0200

    fix: Frontend ci job
     - Fixed frontend linter job

--- frontend/src/ConfigurableAIAssistance.tsx
+++ frontend/src/ConfigurableAIAssistance.tsx
@@ -300,7 +300,7 @@ const ConfigurableAIAssistance = ({
     } finally {
       setIsLoading(false);
     }
-  }, [additionalProps, id]);
+  }, [additionalProps, id, intl]);
 
   const handleOpenSidebar = useCallback(() => {
     setOpenSidebarSignal((prev) => prev + 1);
