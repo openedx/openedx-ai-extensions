9a883351d088878a529e813a2265734d7399f601
henrrypg <henrry.pulgarin@edunext.co>
2026-04-01 10:53:08 -0500

    fix: address comments

--- backend/openedx_ai_extensions/receivers.py
+++ backend/openedx_ai_extensions/receivers.py
@@ -26,16 +26,15 @@ def handle_ai_orchestration_requested(sender, ai_orchestration_request, **kwargs
 
     # Run the orchestrator with the provided input data
     try:
-        user = User.objects.get(id=ai_orchestration_request.user_id) if ai_orchestration_request.user_id else None
-        context = {
-            k: v
-            for k, v in {
-                "course_id": ai_orchestration_request.course_id if ai_orchestration_request.course_id else None,
-                "location_id": ai_orchestration_request.location_id if ai_orchestration_request.location_id else None,
-                "ui_slot_selector_id": ai_orchestration_request.ui_slot_selector_id,
-            }.items()
-            if v is not None
-        }
+        user = User.objects.get(id=ai_orchestration_request.user_id)
+        def compact(d: dict) -> dict:
+            return {k: v for k, v in d.items() if v is not None}
+
+        context = compact({
+            "course_id": ai_orchestration_request.course_id,
+            "location_id": ai_orchestration_request.location_id,
+            "ui_slot_selector_id": ai_orchestration_request.ui_slot_selector_id,
+        })
 
         workflow = AIWorkflowScope.get_profile(**context)
         if workflow is None:
@@ -44,13 +43,12 @@ def handle_ai_orchestration_requested(sender, ai_orchestration_request, **kwargs
                 context,
             )
             return
-        log.info("Found workflow profile for orchestration request. Context: %s", context)
-        result = workflow.execute(
+        workflow.execute(
             user_input=ai_orchestration_request.user_input,
             action=ai_orchestration_request.action,
             user=user,
             running_context=context,
         )
-        log.info("Successfully ran orchestrator for workflow. Context: %s", context)
     except Exception:
         log.exception("Error running orchestrator for workflow")
+        raise
